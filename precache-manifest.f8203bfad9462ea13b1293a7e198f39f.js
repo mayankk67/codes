9999faf14143680b61dbdd7793546795769f63dc
mak kumar <mak@gmail.com>
2019-11-03 01:08:24 +0530

    Updates

---
+++ precache-manifest.f8203bfad9462ea13b1293a7e198f39f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d554f0e7826f1e7776e388c20e2a5a5a",
+    "url": "/codes/index.html"
+  },
+  {
+    "revision": "5c17a11418043708015d",
+    "url": "/codes/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "461a10570cc5915115b3",
+    "url": "/codes/static/js/2.bc0586ca.chunk.js"
+  },
+  {
+    "revision": "5c17a11418043708015d",
+    "url": "/codes/static/js/main.c7f227ec.chunk.js"
+  },
+  {
+    "revision": "cf0ee2d0812b98c06a98",
+    "url": "/codes/static/js/runtime-main.9f93b499.js"
+  }
+]);
